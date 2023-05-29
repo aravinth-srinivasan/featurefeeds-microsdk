6da15240416cca058c686281f055640810f5ede1
aravinth.srinivasan <aravinth.srinivasan@raweng.com>
2023-05-29 14:29:27 +0530

    local api manager added

--- FeatureFeedsMicroSDK/src/main/java/com/raweng/dfe/microsdk/featurefeeds/manager/LocalApiManager.kt
+++ FeatureFeedsMicroSDK/src/main/java/com/raweng/dfe/microsdk/featurefeeds/manager/LocalApiManager.kt
@@ -2,16 +2,15 @@ package com.raweng.dfe.microsdk.featurefeeds.manager
 
 import com.contentstack.sdk.*
 import com.raweng.dfe.microsdk.featurefeeds.data.FeatureFeedRepositoryImpl
-import com.raweng.dfe.microsdk.featurefeeds.data.FeatureFeedRepositoryImpl1
+import com.raweng.dfe.microsdk.featurefeeds.model.FeaturedFeedModel
 import com.raweng.dfe.microsdk.featurefeeds.utils.MicroResult
-import io.reactivex.rxjava3.core.Flowable
-import com.raweng.dfe.microsdk.featurefeeds.model.FeatureFeedModel.Entry as LocalResponseEntry
+import io.reactivex.rxjava3.core.Single
 
 
 class LocalApiManager(private val stack: Stack) {
 
-    fun fetchFeatureFeed(contentType: String): Flowable<MicroResult<ArrayList<LocalResponseEntry>>> {
-        val repository = FeatureFeedRepositoryImpl1(stack)
-        return repository.getFeatureFeeds(contentType)
+    fun fetchFeatureFeed(csContentType: String): Single<MicroResult<List<FeaturedFeedModel>>> {
+        val repository = FeatureFeedRepositoryImpl(stack)
+        return repository.getFeatureFeeds(csContentType)
     }
 }
